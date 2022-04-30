00e7b9ae0ccbf9e8bb0f754b517ae6340e7308b5
Hermawan <hermawangan39@gmail.com>
2022-04-30 16:52:20 +0700

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Nav />}>
+              <Route index element={<LoginPage />} />
+              <Route path="home" element={<Home />} />
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
