137db62b7623bb9c2073ed2f0bb0a22e562aea98
Hermawan <hermawangan39@gmail.com>
2022-05-01 19:02:52 +0700

    added a link to bookmaks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { IoMdBookmark } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <>
       <nav>
         <span>B3K3N</span>
+        <Link to="bookmark">
+          <IoMdBookmark />
+        </Link>
       </nav>
       <Outlet />
     </>
