43c787d41ee2cdecefabbb58204b315fa7556c31
Hermawan <hermawangan39@gmail.com>
2022-05-02 17:15:56 +0700

    addedd a conditional rendering and style fully implemente

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { IoMdBookmark } from "react-icons/io";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Nav() {
+  const location = useLocation();
+  const pattern = new RegExp("[home||books]");
+
   return (
     <>
-      <nav>
-        <span>B3K3N</span>
-        <Link to="bookmark">
-          <IoMdBookmark />
+      <nav
+        className={`${
+          pattern.test(location.pathname)
+            ? " justify-between "
+            : "justify-center"
+        } flex items-center  m-5  md:mx-10 lg:mx-24`}
+      >
+        <Link to="/">
+          <span className="font-bold text-3xl lg:text-4xl">B3K3N</span>
         </Link>
+        {pattern.test(location.pathname) ? (
+          <div className=" flex items-center text-lg md:text-xl">
+            <Link to="home">
+              <p className="pr-5 md:pr-10 lg:pr-16">Home</p>
+            </Link>
+            <Link to="bookmark">
+              <IoMdBookmark />
+            </Link>
+          </div>
+        ) : null}
       </nav>
       <Outlet />
     </>
