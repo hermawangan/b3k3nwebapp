820c341719283e510da1681023ce83819db95ff6
Hermawan <hermawangan39@gmail.com>
2022-05-03 17:14:40 +0700

    change fetching method

--- src/components/SearchBooks.jsx
+++ src/components/SearchBooks.jsx
@@ -1,11 +1,29 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { fetchBooks } from "../redux";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function SearchBooks({ books, fetchBooks, search }) {
+function SearchBooks({ search }) {
+  const [books, setBooks] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [searchResult, setSearchResult] = useState(false);
+
   useEffect(() => {
-    fetchBooks();
+    axios
+      .get("fee-assessment-books/?categoryId=1")
+      .then((response) => {
+        const data = response.data;
+        setBooks(data);
+        setLoading(false);
+        setErrMsg("");
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        setBooks([]);
+        setErrMsg(errorMsg);
+        setLoading(false);
+      });
   }, []);
+
   const filteredBooks =
     search.length === 0
       ? []
@@ -17,32 +35,25 @@ function SearchBooks({ books, fetchBooks, search }) {
 
   return (
     <div className="absolute h-52 left-[4.5%] w-11/12 bg-white border-2  border-gray-800 overflow-auto  rounded-sm md:w-3/4 md:left-[12.5%] md:h-60 lg:w-1/2 lg:left-[25%]">
-      <ul className="">
-        {filteredBooks.map((book) => {
-          return (
-            <li key={book.id}>
-              <span className="text-lg font-semibold md:text-xl">
-                {book.authors[0]}: {book.title}
-              </span>
-            </li>
-          );
-        })}
-      </ul>
+      {loading ? (
+        <p>Searching...</p>
+      ) : errMsg !== "" ? (
+        <p>{errMsg}</p>
+      ) : (
+        <ul>
+          {filteredBooks.map((book) => {
+            return (
+              <li key={book.id}>
+                <span className="text-lg font-semibold md:text-xl">
+                  {book.authors[0]}: {book.title}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    books: state.books.books,
-    loading: state.books.loading,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchBooks: () => dispatch(fetchBooks()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchBooks);
+export default SearchBooks;
