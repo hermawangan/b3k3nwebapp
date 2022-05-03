61791acb23c069db028ed99199cac6cd12a50072
Hermawan <hermawangan39@gmail.com>
2022-05-03 17:15:34 +0700

    adding background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
       backgroundImage: {
         "book-background": "url('/public/book-background.jpg')",
       },
+      colors: {
+        lightGrey: "rgba(0, 0, 0, 0.87)",
+        softGrey: "rgba(0, 0, 0, 0.04)",
+        selectedGrey: "rgba(0, 0, 0, 0.08)",
+      },
     },
   },
   plugins: [],
